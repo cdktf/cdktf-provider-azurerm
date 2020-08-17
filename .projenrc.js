135ec2744328b87d1e6adb5ee0aa7a02c105427a
Sebastian Korfmann <sebastian@korfmann.net>
2020-08-17 13:46:57 +0200

    Initial import

---
+++ .projenrc.js
@@ -0,0 +1,8 @@
+const { CdktfProviderProject } = require('@cdktf/provider-project');
+const { Semver } = require('projen');
+
+const project = new CdktfProviderProject({
+  terraformProvider: "azurerm@~> 2.0.0"
+});
+
+project.synth();
