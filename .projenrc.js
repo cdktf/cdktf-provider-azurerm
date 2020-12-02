d4df8d7393b3568d85920c514743974732442987
Sebastian Korfmann <sebastian@korfmann.net>
2020-12-02 14:46:28 +0100

    Track 2.x rather than 2.0.x

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,7 @@ const { CdktfProviderProject } = require('@cdktf/provider-project');
 const { Semver } = require('projen');
 
 const project = new CdktfProviderProject({
-  terraformProvider: "azurerm@~> 2.0.0"
+  terraformProvider: "azurerm@~> 2.0"
 });
 
 project.synth();
