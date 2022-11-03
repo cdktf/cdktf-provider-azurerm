3d03eb8e0a2d7a9704d62ac208c0f44f0f0a3666
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2022-11-03 01:54:52 +0100

    chore(deps): upgrade dependencies (#1230)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/cdktf/cdktf-provider-azurerm/actions/runs/3382130609
    
    ------
    
    *Automatically created by projen via the "upgrade-main" workflow*

--- scripts/check-for-upgrades.js
+++ scripts/check-for-upgrades.js
@@ -11,7 +11,7 @@ const actions = require("@actions/core");
 
 // set by the projen file that generates this script
 const CONSTRAINT = "~> 3.10";
-const FQ_PROVIDER_NAME = "azurerm";
+const FQ_PROVIDER_NAME = "hashicorp/azurerm";
 
 (async function main() {
   if (await newerMatchingProviderVersionExists()) {
