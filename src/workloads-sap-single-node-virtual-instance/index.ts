cd995827a1fa48a6d9bd64ea960f75ef61594a72
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-08 05:58:11 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/workloads-sap-single-node-virtual-instance/index.ts
+++ src/workloads-sap-single-node-virtual-instance/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/azurerm/4.1.0/docs/resources/workloads_sap_single_node_virtual_instance
 // generated from terraform resource schema
 
