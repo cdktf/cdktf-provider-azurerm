b90a17ed123fd4808c6de92afe5f91f731be7058
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-02-21 04:03:19 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/servicebus-subscription-rule/index.ts
+++ src/servicebus-subscription-rule/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/azurerm/4.20.0/docs/resources/servicebus_subscription_rule
 // generated from terraform resource schema
 
