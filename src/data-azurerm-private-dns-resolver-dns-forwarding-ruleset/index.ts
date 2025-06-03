068ff31c2de23acfed685ef37b9f3a9b17547138
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 12:00:52 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4c005a5

--- src/data-azurerm-private-dns-resolver-dns-forwarding-ruleset/index.ts
+++ src/data-azurerm-private-dns-resolver-dns-forwarding-ruleset/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/azurerm/4.31.0/docs/data-sources/private_dns_resolver_dns_forwarding_ruleset
 // generated from terraform resource schema
 
