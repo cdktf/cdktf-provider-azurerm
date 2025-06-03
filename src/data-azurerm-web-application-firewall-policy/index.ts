48017061ff44d1ea4a4a05397e9f5b2b740efd3f
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 15:40:52 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-azurerm-web-application-firewall-policy/index.ts
+++ src/data-azurerm-web-application-firewall-policy/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/azurerm/4.31.0/docs/data-sources/web_application_firewall_policy
 // generated from terraform resource schema
 
