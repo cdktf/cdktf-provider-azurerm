d7b44dc7edcbd0e3023f5650dfac95f9104e1815
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 14:38:48 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4d35a18

--- src/virtual-desktop-host-pool/index.ts
+++ src/virtual-desktop-host-pool/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/azurerm/4.31.0/docs/resources/virtual_desktop_host_pool
 // generated from terraform resource schema
 
